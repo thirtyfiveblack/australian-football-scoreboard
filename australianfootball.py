3417cf6400a67d3bbd0f2f78e8e5f490f467d653
Andrew Howard <thirtyfiveblack@gmail.com>
2026-05-30 18:34:03 +1000

    Update australianfootball.py

--- australianfootball.py
+++ australianfootball.py
@@ -325,6 +325,8 @@ class AustralianFootballLive(AustralianFootball, SportsLive):
 
                     if away_text:
 #                        away_record_x = 3
+                        away_record_bbox = draw_overlay.textbbox((0, 0), away_text, font=record_font)
+                        away_record_width = away_record_bbox[2] - away_record_bbox[0]
                         away_record_x = display_width - away_record_width - 3
                         self.logger.debug(f"Drawing away ranking '{away_text}' at ({away_record_x}, {record_y})")
                         self._draw_text_with_outline(draw_overlay, away_text, (away_record_x, record_y), record_font)
@@ -352,8 +354,8 @@ class AustralianFootballLive(AustralianFootball, SportsLive):
                         home_text = ''
 
                     if home_text:
-                        home_record_bbox = draw_overlay.textbbox((0, 0), home_text, font=record_font)
-                        home_record_width = home_record_bbox[2] - home_record_bbox[0]
+#                        home_record_bbox = draw_overlay.textbbox((0, 0), home_text, font=record_font)
+#                        home_record_width = home_record_bbox[2] - home_record_bbox[0]
 #                        home_record_x = display_width - home_record_width - 3
                         home_record_x = 3
                         self.logger.debug(f"Drawing home ranking '{home_text}' at ({home_record_x}, {record_y})")
