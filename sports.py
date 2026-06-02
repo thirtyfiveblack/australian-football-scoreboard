fec28955bdf3e00680436e828f59dfebf31f50a5
Andrew Howard <thirtyfiveblack@gmail.com>
2026-06-02 20:16:25 +1000

    Update sports.py

--- sports.py
+++ sports.py
@@ -2205,13 +2205,13 @@ class SportsRecent(SportsCore):
 
 
             # Draw Leaders if available
-            home_goals_number = game.get("home_goals_leader_number","0")
-            home_goals_name = game.get("home_goals_leader_name","A")
-            home_goals_text = f"Goals: {home_goals_name} ({home_goals_number})"
-            home_goals_width = draw_overlay.textlength(home_goals_text, font=self.fonts["status"])
-            home_goals_x = (display_width - home_goals_width) // 2
-            home_goals_y = (display_height // 2) - 3
-            self._draw_text_with_outline(draw_overlay, home_goals_text, (home_goals_x, home_goals_y), self.fonts["status"])
+            #home_goals_number = game.get("home_goals_leader_number","0")
+            #home_goals_name = game.get("home_goals_leader_name","A")
+            #home_goals_text = f"Goals: {home_goals_name} ({home_goals_number})"
+            #home_goals_width = draw_overlay.textlength(home_goals_text, font=self.fonts["status"])
+            #home_goals_x = (display_width - home_goals_width) // 2
+            #home_goals_y = (display_height // 2) - 3
+            #self._draw_text_with_outline(draw_overlay, home_goals_text, (home_goals_x, home_goals_y), self.fonts["status"])
             #self.logger.info(f"Home Leaders: {home_goals_number} goals, : {home_goals_name}")
             
             # "Final" text (Top center) with layout offsets
