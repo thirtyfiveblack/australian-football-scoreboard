11b9b578afe22b3fd41e1a2b5c71f9a7dde48a8a
Andrew Howard <thirtyfiveblack@gmail.com>
2026-06-02 14:30:07 +1000

    Update game_renderer.py

--- game_renderer.py
+++ game_renderer.py
@@ -356,6 +356,15 @@ class GameRenderer:
         score_x = (self.display_width - score_width) // 2
         score_y = (self.display_height // 2) - 3
         self._draw_text_with_outline(draw_overlay, score_text, (score_x, score_y), self.fonts['score'])
+
+        # Draw Leaders if available
+        home_goals_number = game.get('home_goals_leader_number',0)
+        home_goals_name = game.get('home_goals_leader_name',"")
+        home_goals_text = f"Goals: {home_goals_name} ({home_goals_number})"
+        home_goals_width = draw_overlay.textlength(home_goals_text, font=self.fonts['score'])
+        home_goals_x = (display_width - home_goals_width) // 2
+        home_goals_y = (display_height // 2) - 3
+        self._draw_text_with_outline(draw_overlay, home_goals_text, (home_goals_x, home_goals_y), self.fonts['score'])
         
         # Draw period/status based on game type
         if game_type == "live":
