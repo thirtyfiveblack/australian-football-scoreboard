4e5b9902f1b0a7374d44cc6d0e1bfa1ae0f69fb8
Andrew Howard <thirtyfiveblack@gmail.com>
2026-06-11 19:58:03 +1000

    Update australianfootball_helpers.py

--- australianfootball_helpers.py
+++ australianfootball_helpers.py
@@ -115,9 +115,9 @@ class AustralianFootballHelpers:
             # Extract team stats leaders
             home_team_leaders = home_team.get('leaders', [])
             home_team_goals = next((c for c in home_team_leaders if c.get('name') == 'goals'), None)
-            home_team_goals = home_team_goals.get('leaders', [{}])
+            home_team_goals = home_team_goals.get('leaders', [{}])[0]
             home_team_disposals = next((c for c in home_team_leaders if c.get('name') == 'disposals'), None)
-            home_team_disposals = home_team_disposals.get('leaders', [{}])
+            home_team_disposals = home_team_disposals.get('leaders', [{}])[0]
             
             # Build game details
             details = {
