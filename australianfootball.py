81eace9ab691a847be671e3ab69147b72ae02ecd
Andrew Howard <thirtyfiveblack@gmail.com>
2026-06-11 19:31:34 +1000

    Update australianfootball.py

--- australianfootball.py
+++ australianfootball.py
@@ -277,14 +277,14 @@ class AustralianFootballLive(AustralianFootball, SportsLive):
             self._draw_text_with_outline(draw_overlay, score_text, (score_x, score_y), self.fonts['score'])
             
             # Draw Leaders if available
-            #home_goals_number = game.get('home_goals_leader_number',0)
-            #home_goals_name = game.get('home_goals_leader_name',"")
-            #home_goals_text = f"Goals: {home_goals_name} ({home_goals_number})"
-            #home_goals_width = draw_overlay.textlength(home_goals_text, font=self.fonts['score'])
-            #home_goals_x = (display_width - home_goals_width) // 2
-            #home_goals_y = (display_height // 2) - 3
-            #self._draw_text_with_outline(draw_overlay, home_goals_text, (home_goals_x, home_goals_y), self.fonts['score'])
-
+            home_goals_number = str(game.get('home_goals_number',"0"))
+            home_goals_name = str(game.get('home_goals_name',""))
+            home_goals_text = f"Goals: {home_goals_name} ({home_goals_number})"
+            home_goals_width = draw_overlay.textlength(home_goals_text, font=self.fonts['score'])
+            home_goals_x = (self.display_width - home_goals_width) // 2
+            home_goals_y = (self.display_height // 2) - 3
+            self._draw_text_with_outline(draw_overlay, home_goals_text, (home_goals_x, home_goals_y), self.fonts['score'])
+            self.logger.info(f"Stats leaders home {home_goals_name} ({home_goals_number}) data from ESPN API...")
 
             # Draw odds if available
             if 'odds' in game and game['odds']:
